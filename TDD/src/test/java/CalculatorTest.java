eb977a147f8a2ad66bbb8bb04bee41c1eff2d236
Xodein <eraykrdg1@gmail.com>
2023-05-26 11:24:57 +0300

    CREATED

---
+++ TDD/src/test/java/CalculatorTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10,5,2",
+            "10,2.5,4",
+            "12.5,2.5,5",
+            "10,4,2.5",
+            "12.5,5,2.5"
+    })
+    void dataDrivenDivisonTest(double a, double b, double expected){
+        assertEquals(expected, Calculator.divide(a, b),
+                "The result of the division is incorrect");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"12.5, 0.0"})
+    void dataDrivenDivisonExceptionTest(double a, double b){
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(a, b),
+                "Divide by zero exception expected."
+        );
+    }
+}
